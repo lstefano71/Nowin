1e88c38fe12c272f34387774fb867e69ba2c637f
stf@apl.it <stf@apl.it>
2015-08-20 16:36:47 +0200

    static files

--- OwinHostingSample/Program.cs
+++ OwinHostingSample/Program.cs
@@ -2,42 +2,47 @@
 using System.Threading.Tasks;
 using Microsoft.Owin.Hosting;
 using Owin;
+using Microsoft.Owin.StaticFiles;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin;
 
 namespace OwinHostingSample
 {
-    static class Program
-    {
-        static void Main(string[] args)
-        {
-            var options = new StartOptions
-            {
-                ServerFactory = "Nowin",
-                Port = 8080
-            };
+	static class Program
+	{
+		static void Main(string[] args)
+		{
+			var options = new StartOptions {
+				ServerFactory = "Nowin",
+				Port = 8080
+			};
 
-            using (WebApp.Start<Startup>(options))
-            {
-                Console.WriteLine("Running a http server on port 8080");
-                Console.ReadKey();
-            }
-        }
-    }
+			using (WebApp.Start<Startup>(options)) {
+				Console.WriteLine("Running a http server on port 8080");
+				Console.ReadKey();
+			}
+		}
+	}
 
-    public class Startup
-    {
-        public void Configuration(IAppBuilder app)
-        {
-            app.Run(context =>
-            {
-                if (context.Request.Path.Value == "/")
-                {
-                    context.Response.ContentType = "text/plain";
-                    return context.Response.WriteAsync("Hello World!");
-                }
+	public class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			app.UseFileServer(new FileServerOptions {
+				FileSystem = new PhysicalFileSystem("."),
+				RequestPath = new PathString("/files"),
+				EnableDirectoryBrowsing = true
+			});
 
-                context.Response.StatusCode = 404;
-                return Task.Delay(0);
-            });
-        }
-    }
+			app.Run(context => {
+				if (context.Request.Path.Value == "/") {
+					context.Response.ContentType = "text/plain";
+					return context.Response.WriteAsync("Hello World! It's now " + DateTime.Now);
+				}
+
+				context.Response.StatusCode = 404;
+				return Task.Delay(0);
+			});
+		}
+	}
 }
